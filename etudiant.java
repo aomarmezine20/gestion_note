18b96bb1821cef0d5042350d69eed6294c39f9f5
aomarmezine20 <aomar.mezine@edu.uca.ma>
2022-04-15 16:11:24 +0000

    etudiant class

--- etudiant.java
+++ etudiant.java
@@ -1,5 +1,27 @@
 package gestion_note;
 
 public class etudiant {
-    private int id;
+	private int id;
+	private String nom;
+	private double note;
+	public etudiant(int id ,String nom) {
+		this.id = 10;
+	}
+	public String toString()
+	{
+		return this.nom +":" + this.note;
+	}
+	public boolean equals(Object obj)
+	{
+		if (! (obj instanceof etudiant) ){
+			return false;
+		}
+	return	((etudiant)obj).id==this.id;
+	}
+	public boolean compareTo(etudiant e)
+	{
+		if(this.note==e.note) return true;
+		return false;
+
+	}
 }
